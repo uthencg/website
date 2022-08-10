0911dcaee273347e120079f7a71437627cd08b42
uthencg <38644588+uthencg@users.noreply.github.com>
2022-08-11 02:49:00 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my suchart app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by suchart editing <code>pages/index.js</code>
         </p>
       </main>
 
